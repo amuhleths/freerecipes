daef02709295f107c9aedb33a05f4ee0c1f6b4e2
Alvaro Muhlethaler <alvaro.muhlethaler@gmail.com>
2010-11-01 12:02:14 -0200

    App with recipes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freerecipes::Application.config.secret_token = '442fed0a4b325462245f1a49a438c376900bab65cd84d53dca442d2bb9bd8e1c0ed1f3e626368ee804cf201f43a62f8a0da2797058908186870ada118a5f9a79'
